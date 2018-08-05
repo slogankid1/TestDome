1ec056b2d7fc3ea1acbf0435e43d0fc78b120fb3
slogankid1 <slogankid1@users.noreply.github.com>
2018-08-05 18:54:31 +0100

    Playlist
    Passes 1 of 4 tests on TestDome

---
+++ Playlist/Playlist/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Song
+{
+    private string name;
+    public Song NextSong { get; set; }
+
+    public Song(string name)
+    {
+        this.name = name;
+    }
+
+    public bool IsRepeatingPlaylist()
+    {
+
+        while (NextSong != null)
+        {
+            if (NextSong.name == name) { return true; }
+            NextSong = NextSong.NextSong;
+        }
+
+        return false;
+    }
+
+    public static void Main(string[] args)
+    {
+        Song first = new Song("Hello");
+        Song second = new Song("Eye of the tiger");
+
+        first.NextSong = second;
+        second.NextSong = first;
+
+        Console.WriteLine(first.IsRepeatingPlaylist());
+        Console.ReadKey();
+    }
+}
