7dd5b4964fc52765d41261a1bbda01358f784a32
slogankid1 <slogankid1@users.noreply.github.com>
2018-08-05 18:37:26 +0100

    Palindrome Solution

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        int wordLength = word.Length;
+        word = word.ToLower();
+        int i = 0;
+
+        if (wordLength == 0) { return false; }
+
+        foreach (char c in word)
+        {
+            if (c != word[(wordLength - 1) - i]) { return false; }
+            i++;
+
+            if (i == wordLength / 2) { break; }
+        }
+
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+        Console.ReadKey();
+    }
+}
